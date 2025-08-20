ba40f0f211306011540457215b13df09393ad282
Fondue28 <nattasasi.fong@gmail.com>
2025-08-20 15:39:15 +0700

    Added APIs

---
+++ app/vat/page.js
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>VAT Calculator</h1>
+            <p>
+                Price: <input type="number" placeholder="Enter price" />
+                <br/>
+                <button>Calculate VAT</button>
+            </p>
+        </div>
+    );
+}
